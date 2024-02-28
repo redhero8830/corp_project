a10c31a49a8dc61546c8884eb92f9a3327bfae92
Mark Kwon <redhero8830@gmail.com>
2024-02-29 02:30:28 +0900

    hihi

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
+// require('dotenv').config();
+// const API_KEY = process.env.API_KEY;
 const API_KEY = "9fab3ef1d371e73ccc5e7b77cf1f54701ca89336";
-const CORP_CODE = ["00126380", "00434003", "00434456"]; // 삼성전자 코드
-const YEAR = "2022";
-const REPORT_CODE = "11011"; // 11011: 사업보고서 (나머지는 반기 / 분기 보고서)
+const CORP_CODE = ["00126380", "00126380", "00126380", "00126380", "00126380"]; // 삼성전자 코드
+const YEAR = "2020";
+const REPORT_CODE = "11013"; // 11011: 사업보고서 (나머지는 반기 / 분기 보고서)
 
 const CORS_LINK = "https://cors-anywhere.herokuapp.com/";
-const API_LINK = "https://opendart.fss.or.kr/api/fnlttSinglAcnt.json";
-const CORP_API_LINK = "https://opendart.fss.or.kr/api/company.json";
+const CORP_NAME_API_LINK = "https://opendart.fss.or.kr/api/company.json";
+const CORP_INFO_API_LINK = "https://opendart.fss.or.kr/api/fnlttSinglAcnt.json";
 
 let corpInfos = [];
+
+const favoritClickBtn = document.querySelector(".p-favorite-button");
+let isLiked = false;
+
+favoritClickBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (!isLiked) {
+    favoritClickBtn.src = "asset/like.png";
+    isLiked = true;
+  } else {
+    favoritClickBtn.src = "asset/no-like.png";
+    isLiked = false;
+  }
+});
 
 const popularGetCorpInfo = async () => {
   for (let corp of CORP_CODE) {
     let corpName_url = new URL(
-      `${CORS_LINK}${CORP_API_LINK}?crtfc_key=${API_KEY}&corp_code=${corp}`
+      `${CORS_LINK}${CORP_NAME_API_LINK}?crtfc_key=${API_KEY}&corp_code=${corp}`
     );
     let corpInfo_url = new URL(
-      `${CORS_LINK}${API_LINK}?crtfc_key=${API_KEY}&corp_code=${corp}&bsns_year=${YEAR}&reprt_code=${REPORT_CODE}`
+      `${CORS_LINK}${CORP_INFO_API_LINK}?crtfc_key=${API_KEY}&corp_code=${corp}&bsns_year=${YEAR}&reprt_code=${REPORT_CODE}`
     );
     const responseCorpName = await fetch(corpName_url);
     const responseCorpInfo = await fetch(corpInfo_url);
@@ -46,14 +62,17 @@ const popularGetCorpInfo = async () => {
   }
 };
 
-// popularGetCorpInfo();
+popularGetCorpInfo();
 
 const render = () => {
   let result = "";
   for (let info of corpInfos) {
-    result += `<div class="p-corp">
+    result += `<a class="p-corp" href="https://m.stock.naver.com/domestic/stock/005930/total">
                 <div class="p-corpName">
-                    <span><img src="asset/no-like.png"></span>${info.corpName}
+                    <span>
+                        <img class="p-favorite-button" src="asset/no-like.png">
+                    </span>
+                    ${info.corpName}
                 </div>
                 <div class="p-sales">
                     ${Math.ceil(info.sales / 1000000000000)}조원
@@ -71,7 +90,7 @@ const render = () => {
                 <div class="p-netIncome">
                     ${Math.ceil(info.netIncome / 1000000000000)}조원
                 </div>
-            </div>`;
+            </a>`;
   }
-  document.querySelector(".p-listContent").innerHTML = result;
+  document.querySelectorAll(".p-listContent")[1].innerHTML = result;
 };
